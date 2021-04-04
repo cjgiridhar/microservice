b61c6b40fb5ebcd1da20698ad78ed5e05ac0b641
Chetan Giridhar <chetangiridhar@Chetans-MacBook-Pro.local>
2021-04-04 10:16:41 +0530

    First commit

---
+++ customer-service/src/main/java/com/chetangiridhar/customer/CustomerServiceApplication.java
@@ -0,0 +1,19 @@
+package com.chetangiridhar.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
